e0bb0100af5499515173e693cc27ef493d889889
Ellen Almeida <ruthgaah13@gmail.com>
2023-11-07 20:17:53 -0300

    .

---
+++ forca.py
@@ -0,0 +1,9 @@
+def jogar():
+    print("*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*")
+    print("***Bem vindX ao jogo da Forca!***")
+    print("*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*")
+
+    print("Acabou o joguinho")
+
+if(__name__ == "__main__"):
+    jogar()
--- jogos.py
+++ jogos.py
@@ -1,6 +1,21 @@
-nome = "Nico"
-sobrenome = "Steppat"
-print(nome + sobrenome)
-print(nome + sobrenome, sep="_")
+import forca
+import adivinhacao
 
-print("ola", "mundo")
+def escolhe_jogo():
+    print("*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*")
+    print("       Escolha o seu jogo!       ")
+    print("*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*")
+
+    print("(1) Forca (2) Adivinhação")
+
+    jogo = int(input("Qual jogoquinho tu quer jogar? "))
+
+    if(jogo == 1):
+        print("Jogando forca, escolhe esse que é legalzinho")
+        forca.jogar()
+    elif(jogo == 2):
+        print("Jogando adivinhação, esocolhe esse nao porque nao gosto de adivinhar")
+        adivinhacao.jogar()
+
+if(__name__ == "__main__"):
+    escolhe_jogo()
